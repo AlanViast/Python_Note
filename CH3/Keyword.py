5481d22e77488ae3ba53640920f305508bdb814c
AlanViast <alan9510@163.com>
2015-03-26 22:47:53 +0800

    看到第三章P48

---
+++ CH3/Keyword.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+
+
+import keyword
+
+keywordStr = raw_input("Enter a string > ")
+
+if keyword.iskeyword(keywordStr) :
+    print "`%s` is a keyword" %(keywordStr)
+else :
+    print "`%s` not a keyword" %(keywordStr)
